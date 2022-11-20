6bba5f151f12f059d70ba0bdc630678a8a106290
Chiheb mejri <chihebmjrr@gmail.com>
2022-11-20 15:00:45 +0100

    multiSearch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import { Users } from './users';
 import {useState} from 'react'
+import Table from './table';
 
 function App() {
-  const [query, setQuery] = useState("")
-  console.log(Users.filter(user=>user.first_name.toLowerCase().includes(query)));
+  const [query, setQuery] = useState("");
+  const keys = ["first_name", "last_name", "email"];
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
   return (
     <>
     <div className='container'>
@@ -12,15 +18,16 @@ function App() {
        type='text' 
        className ='search' 
        onChange={(e)=> setQuery(e.target.value)}/>
-      <ul className='list'>
+      {/* <ul className='list'>
         {Users.filter(user=>user.first_name.toLowerCase().includes(query)).map((user,id)=>(
           <li key={id} className='listeItem'>{user.first_name}</li>
         ))}
        
 
-      </ul>
+      </ul> */}
 
     </div>
+    <Table data= {search(Users)}/>
 
     
     </>
